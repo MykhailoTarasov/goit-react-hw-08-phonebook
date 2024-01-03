d72d18b2ff973daf60b83cce257a63f7fc9a6612
MykhailoTarasov <123101959+MykhailoTarasov@users.noreply.github.com>
2024-01-03 11:18:00 +0200

    play

--- src/components/Openning/Openning.Styled.js
+++ src/components/Openning/Openning.Styled.js
@@ -1,11 +1,18 @@
 import styled from 'styled-components';
 import { NavLink } from "react-router-dom";
 
+export const Container = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-contant: center;
+`
+
 export const Text = styled.p`
 text-shadow: grey 2px 2px 5px;
-text-align: center;
+
 `
 
 export const Link = styled(NavLink)`
-text-align: center;
+
 `
--- src/components/Openning/Openning.js
+++ src/components/Openning/Openning.js
@@ -1,13 +1,13 @@
-import { Link, Text } from "./Openning.Styled";
+import { Container, Link, Text } from "./Openning.Styled";
 
 export const Opening = () => {
     return (
-      <div>
+      <Container>
         <Text>
           It is designed to help your brain not remember numbers, but write them
           down.
         </Text>
         <Link to={'/contacts'}>Add contact</Link>
-      </div>
+      </Container>
     );
   };
