59fa412ecd0a1b05e9ca36d369239ff66402d01c
MykhailoTarasov <123101959+MykhailoTarasov@users.noreply.github.com>
2024-01-03 11:28:45 +0200

    play

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,34 +1,36 @@
-import { CustomModalForm } from "components/CustomerModal/CustomerModal";
-import { Notify } from "notiflix";
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { deleteContact } from "../../redux/contacts/operations";
+import { CustomModalForm } from 'components/CustomerModal/CustomerModal';
+import { Notify } from 'notiflix';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { deleteContact } from '../../redux/contacts/operations';
+import { ButtonItem, List, ListItem, Text } from './Contact.Styled';
 
 export const Contact = ({ id, name, number }) => {
-    const dispatch = useDispatch();
-    const [isModalOpen, setIsModalOpen] = useState(false);
-  
-    const openModal = () => {
-      setIsModalOpen(true);
-    };
-  
-    const closeModal = () => {
-      setIsModalOpen(false);
-    };
-  
-    const handlerOnClick = id => {
-      dispatch(deleteContact(id));
-      Notify.success(`Contact deleted`);
-    };
-  
-    return (
-      <li>
-        <span>
+  const dispatch = useDispatch();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handlerOnClick = id => {
+    dispatch(deleteContact(id));
+    Notify.success(`Contact deleted`);
+  };
+
+  return (
+    <List>
+      <ListItem>
+        <Text>
           {name}: {number}
-        </span>
-        <button type="button" onClick={() => handlerOnClick(id)}>
+        </Text>
+        <ButtonItem type="button" onClick={() => handlerOnClick(id)}>
           Delete
-        </button>
+        </ButtonItem>
         <button type="button" onClick={openModal}>
           Update
         </button>
@@ -39,6 +41,7 @@ export const Contact = ({ id, name, number }) => {
             userId={id}
           />
         )}
-      </li>
-    );
-  };
+      </ListItem>
+    </List>
+  );
+};
