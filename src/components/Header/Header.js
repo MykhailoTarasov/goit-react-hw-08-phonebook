985277ea27b6ab82276355a71b4e13af17a20746
MykhailoTarasov <123101959+MykhailoTarasov@users.noreply.github.com>
2024-01-03 11:50:29 +0200

    play

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import { AuthNav } from "components/AuthNav/AuthNav";
+import { Container } from "components/Layout";
 import { Navigation } from "components/Navigation/Navigation";
 import { UserMenu } from "components/UserMenu/UserMenu";
 import { useAuth } from "hooks";
@@ -8,7 +9,7 @@ import { Outlet } from "react-router-dom";
 export const Header = () => {
   const { isLoggedIn } = useAuth();
   return (
-    <div>
+    <Container>
       <header>
         <Navigation />
         {isLoggedIn ? <UserMenu /> : <AuthNav />}
@@ -16,6 +17,6 @@ export const Header = () => {
       <Suspense fallback={null}>
         <Outlet />
       </Suspense>
-    </div>
+    </Container>
   );
 };
